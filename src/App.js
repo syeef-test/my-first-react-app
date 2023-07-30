0f2dadf089896e8b4ef36d7411f6f945400c8901
syeefislam <syeefislam@yahoo.com>
2023-07-30 20:18:10 +0530

    first component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Heading() { 
+  return ( 
+    <h1>This is an h1 heading.</h1> 
+  ) 
+} 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
   );
 }
 
